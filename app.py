f854550755f0d28cd0818728ea722c1790006fe5
tabarak23 <152076025+tabarak23@users.noreply.github.com>
2025-07-06 05:41:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello This is vineesh"
+    return "hajaat rawa is only allah nafa and nuksaan's only muktaar is allah"
+
+@app.route('/healthz')
+def healthz():
+    return jsonify({"status": "ok"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
