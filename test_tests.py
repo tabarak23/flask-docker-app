6df5c4a638e648e5eb547ee58cc27d4bed00f11a
tabarak23 <er.tabarak2000@gmail.com>
2025-07-06 08:26:55 +0530

    complete final commit# Please enter the commit message for your changes. Lines starting

--- test_tests.py
+++ test_tests.py
@@ -9,7 +9,10 @@ def client():
 def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"making my hands dirty on cicd with docker and k8s" in response.data
+    assert (
+    b"making my hands dirty on cicd with docker and k8s"
+    in response.data
+)
 
 def test_healthz(client):
     response = client.get('/healthz')
