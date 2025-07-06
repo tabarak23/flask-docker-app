58c52b3e8caba549de7341b10f3342620eeece37
tabarak23 <er.tabarak2000@gmail.com>
2025-07-06 06:21:58 +0530

    first

---
+++ test_app.py
@@ -0,0 +1,19 @@
+# test_app.py
+import pytest
+from app import app  # import your Flask app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"hajaat rawa is only allah nafa" in response.data
+
+def test_healthz(client):
+    response = client.get('/healthz')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data == {"status": "ok"}
