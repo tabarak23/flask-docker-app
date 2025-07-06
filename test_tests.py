2b9165cd8d14a7f3382cf9e4b5d4885c831a0625
tabarak23 <er.tabarak2000@gmail.com>
2025-07-06 08:28:21 +0530

    last

--- test_tests.py
+++ test_tests.py
@@ -10,9 +10,10 @@ def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
     assert (
-    b"making my hands dirty on cicd with docker and k8s"
-    in response.data
-)
+        b"making my hands dirty on cicd with docker and k8s"
+        in response.data
+    )
+
 
 def test_healthz(client):
     response = client.get('/healthz')
