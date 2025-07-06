7ad2ff042ca208efa1d0961ae28b89f21a2780e8
tabarak23 <er.tabarak2000@gmail.com>
2025-07-06 06:40:44 +0530

    w

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return "hajaat rawa is only allah nafa and nuksaan's only muktaar is allah"
 
+
 @app.route('/healthz')
 def healthz():
     return jsonify({"status": "ok"}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
